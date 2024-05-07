be3841e1d8653b12d6d27ac3fbc4bf92e1db6ebc
Pranav K Bhaskar <pkb19032002@gmail.com>
2024-05-07 13:05:23 +0530

    Added category classification stat

--- app/routers/statistics.py
+++ app/routers/statistics.py
@@ -1,3 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pymongo import MongoClient
+from ..database import get_db
 
 router = APIRouter(prefix="/stats", tags=["Statistics"])
+
+
+@router.get("/category-distribution")
+def get_category_distribution(db: MongoClient = Depends(get_db)):
+    categoriesDistribution = dict()
+
+    service_providers = db["serviceProviders"].find()
+
+    for service_provider in service_providers:
+        for service in service_provider.get("services", []):
+            category = service["category"]
+            if category in categoriesDistribution:
+                categoriesDistribution[category] += 1
+            else:
+                categoriesDistribution[category] = 1
+
+    return categoriesDistribution
